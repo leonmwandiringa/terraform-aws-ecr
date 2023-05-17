7718c31ef25f5f7dff8679770f67fc7fb9a6dd1f
leonvodacom <leon.mwandiringa@vcontractor.co.za>
2023-05-17 23:13:03 +0200

    outputs update

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,19 @@
-output "registry_ids" {
-  value       = aws_ecr_repository.default.*.registry_id
-  description = "Registry IDs"
+output "registry_id" {
+  value       = aws_ecr_repository.default[0].registry_id
+  description = "Registry ID"
 }
 
-output "repository_names" {
-  value       = aws_ecr_repository.default.*.name
-  description = "Name of repositories created"
+output "repository_name" {
+  value       = join(",", aws_ecr_repository.default.*.name)
+  description = "Name of first repository created"
 }
 
-output "repository_urls" {
-  value       = aws_ecr_repository.default.*.repository_url
-  description = "URL of repositories created"
+output "repository_url" {
+  value       = join(",", aws_ecr_repository.default.*.repository_url)
+  description = "URL of first repository created"
 }
 
-output "repository_arns" {
-  value       = aws_ecr_repository.default.*.arn
-  description = "ARN of repositories created"
+output "repository_arn" {
+  value       = join(",", aws_ecr_repository.default.*.arn)
+  description = "ARN of first repository created"
 }
